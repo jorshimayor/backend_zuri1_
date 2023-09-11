c61497308ccc790f2c8c029718a6894eaab0b370
Joshua Obafemi <obafemijoshua2020@gmail.com>
2023-09-11 14:12:12 +0100

    Done

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.get("/api", (req, res) => {
   ]
 
   const currentDay = Day[currentDate.getUTCDay()]
-  const utcTime = currentDate.toISOString()
+  const utcTime = currentDate.toISOString().split(".")[0] + "Z"
   const githubFileUrl = "https://github.com/jorshimayor/backend_zuri1_/blob/main/index.js"
   const githubRepoUrl = "https://github.com/jorshimayor/backend_zuri1_"
 
